46cd01b828f7ffd6b3a7ebf70839842bc0a15ec5
md-shamimislam <123378469+md-shamimislam@users.noreply.github.com>
2023-01-23 18:58:57 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$apiaccount = "01XXXXXXX"; // Api Account Number
+$apikey = "XXXXXXX"; // Api Key
+$number = "XXXXXXX"; // TopUp Number
+$amount = "XX"; // TopUp Amount
+$type = "Prepaid"; // Type [Prepaid/Postpaid/Skitto]
+
+$url = "https://sohojpaybd.com/app/api/topup.php?access_id=$apiaccount&access_key=$apikey&number=$number&amount=$amount&type=$type";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+
+?>
